602853ef32336c4ba818415b9d5c92d088b97c32
pradeepsurya <pradeepsurya@live.com>
2023-09-07 03:15:47 -0400

    add admin-dashboard module

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BoardComponent } from './manager-board/board/board.component'
+import { DashboardComponent } from './admin-dashboard/dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: '', component: BoardComponent }
+  { path: '', component: BoardComponent },
+  { path: 'admin-dashboard', component: DashboardComponent },
+
 ];
 
 @NgModule({
