0ea0d9a2484d51251e618f76fecc157d4542fb40
pradeepsurya <pradeepsurya@live.com>
2023-09-08 04:44:04 -0400

    add admin dashboard components

--- src/app/admin-dashboard/admin-dashboard.module.ts
+++ src/app/admin-dashboard/admin-dashboard.module.ts
@@ -1,15 +1,44 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
-
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatTableModule } from '@angular/material/table';
+import { HighchartsChartModule } from 'highcharts-angular';
+import { DashboardService } from './dashboard.service';
+import { AreaComponent } from './area/area.component';
+import { CardComponent } from './card/card.component';
+import { PieComponent } from './pie/pie.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
+import { AdminDashboardRoutingModule } from './admin-dashboard.routing.module';
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    AreaComponent,
+    CardComponent,
+    PieComponent,
+  ],
+  providers: [
+    DashboardService
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    HighchartsChartModule,
+    FlexLayoutModule,
+    MatSidenavModule,
+    MatDividerModule,
+    MatCardModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatIconModule,
+    AdminDashboardRoutingModule
+  ],
+  exports: [
+    DashboardComponent
   ]
 })
 export class AdminDashboardModule { }
