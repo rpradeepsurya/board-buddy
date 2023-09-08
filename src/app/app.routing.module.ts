31b4801d85ebe8e4ba088e9c42a9e7ce7be08efe
pradeepsurya <pradeepsurya@live.com>
2023-09-08 04:43:20 -0400

    add lazy loading admin module

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { BoardComponent } from './manager-board/board/board.component'
-import { DashboardComponent } from './admin-dashboard/dashboard/dashboard.component';
+import { BoardComponent } from './manager-board/board/board.component';
 
 const routes: Routes = [
   { path: '', component: BoardComponent },
-  { path: 'admin-dashboard', component: DashboardComponent },
+  { path: 'admin-dashboard', 
+    loadChildren: () => import('./admin-dashboard/admin-dashboard.module').then(m => m.AdminDashboardModule) },
 
 ];
 
